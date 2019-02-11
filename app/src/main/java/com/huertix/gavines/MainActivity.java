5d5141ba0d30487ec4cca964d032967db5c75810
Angel <dhuerta23@gmail.com>
2019-02-11 10:08:31 +0100

    new endpoint

--- app/src/main/java/com/huertix/gavines/MainActivity.java
+++ app/src/main/java/com/huertix/gavines/MainActivity.java
@@ -29,9 +29,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button recibidorBtn;
     private Button trasteroBtn;
     private Button trescantosPatioBtn;
+    private Button trescantosBuhardillaBtn;
     private TextView recibidorTxt;
     private TextView trasteroTxt;
     private TextView trecantosPatioTxt;
+    private TextView trecantosBuhardillaTxt;
+    private TextView recibidorTxt1;
+    private TextView trasteroTxt1;
+    private TextView trecantosPatioTxt1;
+    private TextView trecantosBuhardillaTxt1;
     private RequestQueue requestQueue;
 
     @Override
@@ -42,20 +48,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         recibidorBtn = (Button) findViewById(R.id.recibidor_btn);
         trasteroBtn = (Button) findViewById(R.id.trastero_btn);
         trescantosPatioBtn = (Button) findViewById(R.id.trescantos_patio_btn);
+        trescantosBuhardillaBtn = (Button) findViewById(R.id.trescantos_buhardilla_btn);
         recibidorTxt = (TextView) findViewById(R.id.recibidor_text_view);
         trasteroTxt = (TextView) findViewById(R.id.trastero_text_view);
         trecantosPatioTxt = (TextView) findViewById(R.id.trescantos_patio_text_view);
+        trecantosBuhardillaTxt = (TextView) findViewById(R.id.trescantos_buhardilla_text_view);
+        recibidorTxt1 = (TextView) findViewById(R.id.recibidor_text_view_1);
+        trasteroTxt1 = (TextView) findViewById(R.id.trastero_text_view_1);
+        trecantosPatioTxt1 = (TextView) findViewById(R.id.trescantos_patio_text_view_1);
+        trecantosBuhardillaTxt1 = (TextView) findViewById(R.id.trescantos_buhardilla_text_view_1);
 
 
         recibidorBtn.setOnClickListener(this);
         trasteroBtn.setOnClickListener(this);
         trescantosPatioBtn.setOnClickListener(this);
+        trescantosBuhardillaBtn.setOnClickListener(this);
 
         requestQueue = Volley.newRequestQueue(this);
 
         recibidorBtn.callOnClick();
         trasteroBtn.callOnClick();
         trescantosPatioBtn.callOnClick();
+        trescantosBuhardillaBtn.callOnClick();
 
     }
 
@@ -68,18 +82,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d(TAG, "requested recibidor");
                 updateValues("gavines-recibidor");
                 recibidorTxt.setText("Loading...");
+                recibidorTxt1.setText("");
 
                 break;
             case R.id.trastero_btn:
                 Log.d(TAG, "requested trastero");
                 updateValues("gavines-trastero");
                 trasteroTxt.setText("Loading...");
+                trasteroTxt.setText("");
 
                 break;
             case R.id.trescantos_patio_btn:
                 Log.d(TAG, "requested trescantos patio");
                 updateValues("trescantos-patio");
                 trecantosPatioTxt.setText("Loading...");
+                trecantosPatioTxt.setText("");
+
+                break;
+
+            case R.id.trescantos_buhardilla_btn:
+                Log.d(TAG, "requested trescantos buhardilla");
+                updateValues("trescantos-buhardilla");
+                trecantosBuhardillaTxt.setText("Loading...");
+                trecantosBuhardillaTxt.setText("");
+
 
                 break;
         }
@@ -98,21 +124,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             Log.d(TAG, response.toString());
                             try {
                                 JSONObject values = response.getJSONArray("with").getJSONObject(0);
+                                JSONObject values1 = response.getJSONArray("with").getJSONObject(4);
                                 String date = values.getString("created").substring(0,19).replace("T"," ");
+                                String date1 = values1.getString("created").substring(0,19).replace("T"," ");
                                 JSONObject content = values.getJSONObject("content");
+                                JSONObject content1 = values.getJSONObject("content");
                                 Double tmp = content.getDouble("temperatura");
+                                Double tmp1 = content1.getDouble("temperatura");
                                 Double humedad = content.getDouble("humedad");
+                                Double humedad1 = content1.getDouble("humedad");
 
                                 if (location.equals("gavines-recibidor")) {
                                     recibidorTxt.setText( date + " TMP: " + Double.toString(tmp) + " HUM: " + Double.toString(humedad) + "%");
+                                    recibidorTxt1.setText( date1 + " TMP: " + Double.toString(tmp1) + " HUM: " + Double.toString(humedad1) + "%");
                                 }
 
                                 if (location.equals("gavines-trastero")) {
                                     trasteroTxt.setText( date + " TMP: " + Double.toString(tmp) + " HUM: " + Double.toString(humedad) + "%");
+                                    trasteroTxt1.setText( date1 + " TMP: " + Double.toString(tmp1) + " HUM: " + Double.toString(humedad1) + "%");
                                 }
 
                                 if (location.equals("trescantos-patio")) {
                                     trecantosPatioTxt.setText( date + " TMP: " + Double.toString(tmp) + " HUM: " + Double.toString(humedad) + "%");
+                                    trecantosPatioTxt1.setText( date1 + " TMP: " + Double.toString(tmp1) + " HUM: " + Double.toString(humedad1) + "%");
+
+                                }
+
+                                if (location.equals("trescantos-buhardilla")) {
+                                    trecantosBuhardillaTxt.setText( date + " TMP: " + Double.toString(tmp) + " HUM: " + Double.toString(humedad) + "%");
+                                    trecantosBuhardillaTxt1.setText( date1 + " TMP: " + Double.toString(tmp1) + " HUM: " + Double.toString(humedad1) + "%");
+
                                 }
 
 
